2e1d5829b6eb5a4375d3ae8c32faff304e2c161e
SharonAbra <abra.sharon@gmail.com>
2021-07-04 12:08:46 +0300

    daily challenge

---
+++ Week2/Day1/Excercises/Excercises in class/Excercise 1/script.js
@@ -0,0 +1,5 @@
+let addressNumber = 50;
+let addressStreet = "Massada";
+let country = "Israel";
+let globalAddress = `I live in ${addressStreet} ${addressNumber}, ${country}.`;
+console.log(globalAddress);
---
+++ Week2/Day1/Excercises/Excercises in class/Excercise 2/script.js
@@ -0,0 +1,4 @@
+let birthYear = 1987;
+let futureYear = 2042;
+let futureAge = futureYear - 1987;
+console.log(`I will be ${futureAge} in ${futureYear}!`)
