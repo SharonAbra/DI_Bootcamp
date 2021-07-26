1428f1d61581f1df14b6b18089e9951b6e92a3c6
SharonAbra <abra.sharon@gmail.com>
2021-07-26 12:56:14 +0300

    exercise xp

--- Week5/Day2/Exercises_at_home/Exercise-XP/script.js
+++ Week5/Day2/Exercises_at_home/Exercise-XP/script.js
@@ -35,32 +35,40 @@ function loadXml() {
   });
 }
 
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My');
-// xhr.responseType = 'json';
-// xhr.send();
-// xhr.onreadystatechange = function() {
-//   if (xhr.readyState == 0) {
-//     console.log("unsent")
-//   } else if(xhr.readyState == 1) {
-//     console.log("open called")
-//   } else if (xhr.readyState == 2) {
-//     console.log("response headers received")
-//   } else if (xhr.readyState == 3) {
-//     console.log("response is loading")
-//   } else if (xhr.readyState == 4) {
-//       console.log("xhr object = ", xhr)
-//       console.log("request complete")
-//       if (xhr.status != 200) {
-//           console.log('error')
-//         } else if (xhr.status === 200) {
-//             let list = document.querySelector(".list");
-//             let giphArrayOfObjects = xhr.response["data"];
-//             giphArrayOfObjects.forEach(object => {
-//               let li = document.createElement("li");
-//               li.innerHTML = object["url"];
-//               list.appendChild(li);
-//             });
-//     }
-//   }
-// }
+/// number 2
+let xhr2 = new XMLHttpRequest();
+xhr2.open('GET', 'https://api.giphy.com/v1/gifs/search?q=sun&limit=10&offset=2&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My');
+xhr2.responseType = 'json';
+xhr2.send();
+
+xhr2.onload = function(){
+  if(xhr2.status != 200){
+    console.log(`Error: ${xhr2.status}: ${xhr2.statusText}`);
+  }
+  else{
+    console.log(xhr2.response);
+    loadXml2(xhr2.response);
+  }
+}
+
+xhr2.onprogress = function(event) {
+  if (event.lengthComputable) {
+    console.log(`Received ${event.loaded} of ${event.total} bytes`);
+  } else {
+    console.log(`Received ${event.loaded} bytes`);
+  }
+};
+
+xhr2.onerror = function() {
+  console.log('Error something wrong###');
+};
+
+function loadXml2() {
+  let list2 = document.querySelector(".list2");
+  let giphArrayOfObjects = xhr2.response["data"];
+  giphArrayOfObjects.forEach(object => {
+  let li = document.createElement("li");
+  li.innerHTML = object["url"];
+  list2.appendChild(li);
+  });
+}
